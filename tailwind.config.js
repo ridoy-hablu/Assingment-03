6fc789dc5c3e3a223df0676a744f07e838fa0358
Ridoy <havexking07@gmail.com>
2024-11-08 18:08:04 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        arial: ['Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
